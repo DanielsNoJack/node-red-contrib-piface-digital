c0dadad589d74383401caf96c6eb3d55b8c3d0ea
Daniel Krhanek <daniel.krhanek@olc.cz>
2019-04-02 10:00:39 +0200

    Added input node for triggered read of input state, Repaired send initial state (need delay after flow start), Reformated

--- piface-digital/rpi-piface-digital.js
+++ piface-digital/rpi-piface-digital.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  **/
 
-module.exports = function(RED) {
+module.exports = function (RED) {
     "use strict";
     var util = require("util");
     var PIFD = require('node-pifacedigital');
@@ -22,106 +22,106 @@ module.exports = function(RED) {
     // Map names of pins to Gordon's gpio PiFace pin numbers
     var pintable = {
         // Physical : WiringPi
-        "PIN 1":0,
-        "PIN 2":1,
-        "PIN 3":2,
-        "PIN 4":3,
-        "PIN 5":4,
-        "PIN 6":5,
-        "PIN 7":6,
-        "PIN 8":7
+        "PIN 1": 0,
+        "PIN 2": 1,
+        "PIN 3": 2,
+        "PIN 4": 3,
+        "PIN 5": 4,
+        "PIN 6": 5,
+        "PIN 7": 6,
+        "PIN 8": 7
     }
-	
-	var hardwaretable = {
-        "Address 0":0,
-        "Address 1":1,
-        "Address 2":2,
-        "Address 3":3		
-		
-	}
+    var hardwaretable = {
+        "Address 0": 0,
+        "Address 1": 1,
+        "Address 2": 2,
+        "Address 3": 3
+    }
 
-	
+
     function PiFACEDigitalInNode(n) {
-        RED.nodes.createNode(this,n);
-        this.hardware = hardwaretable[n.hardware];        
-		this.npin = n.pin;
+        RED.nodes.createNode(this, n);
+        this.hardware = hardwaretable[n.hardware];
+        this.npin = n.pin;
         this.pin = pintable[n.pin];
         this.read = n.read || false;
-        if (this.read) { this.buttonState = -2; }
+        if (this.read) {
+            this.buttonState = -2;
+        }
         var node = this;
-        if (node.pin>=0) {
-           node.pi = new PIFD.PIFaceDigital(node.hardware,true); 
-		   if (node.read)
-		     {
-			 node.val = node.pi.get(node.pin);   
-		     var msg = {topic:"piface/"+node.npin, payload:node.val};
-             node.send(msg);		     
-			 }  
-           		 
-           var callback = function(pin,type){
-             // Type will be 'lohi' or 'hilo'.
-			 if (type=='lohi')
-			   {
-               node.val=1;				   
-			   }
-		      else
-			   {
-			   node.val=0;
-		       }
-			 node.status({fill:"green",shape:"dot",text:node.val});  
-			 var msg = {topic:"piface/"+node.npin, payload:node.val};
-             node.send(msg);
+        if (node.pin >= 0) {
+            node.pi = new PIFD.PIFaceDigital(node.hardware, true);
+            if (node.read) {
+                node.val = node.pi.get(node.pin);
+                var msg = {topic: "piface/" + node.npin, payload: node.val};
+                setTimeout(function () {
+                    node.send(msg);
+                }, 1000);
+            }
+            node.on("input", function (msg) {
+                node.val = node.pi.get(node.pin);
+                var msg = {topic: "piface/" + node.npin, payload: node.val};
+                node.send(msg);
+            });
+
+            var callback = function (pin, type) {
+                node.error("Callback");
+
+                // Type will be 'lohi' or 'hilo'.
+                if (type == 'lohi') {
+                    node.val = 1;
+                } else {
+                    node.val = 0;
+                }
+                node.status({fill: "green", shape: "dot", text: node.val});
+                var msg = {topic: "piface/" + node.npin, payload: node.val};
+                node.send(msg);
             };
-           
-		   node.pi.watch(node.pin,callback);			
-           node.status({fill:"green",shape:"dot",text:node.val});
-                                        
-          }
-        else 
-		  {
-          node.error("Invalid PiFACE pin: "+node.pin);
-		  node.status({fill:"red",shape:"ring",text:"misconfigured"},true);
-          }
-        node.on("close", function() {
-            node.pi.unwatch (node.pin);
+            node.pi.watch(node.pin, callback);
+            node.status({fill: "green", shape: "dot", text: node.val});
+            var msg = {topic: "piface/" + node.npin, payload: node.val};
+            node.send(msg);
+        } else {
+            node.error("Invalid PiFACE pin: " + node.pin);
+            node.status({fill: "red", shape: "ring", text: "misconfigured"}, true);
+        }
+        node.on("close", function () {
+            node.pi.unwatch(node.pin);
         });
     }
 
     function PiFACEDigitalOutNode(n) {
-        RED.nodes.createNode(this,n);
-        this.hardware = hardwaretable[n.hardware];         
+        RED.nodes.createNode(this, n);
+        this.hardware = hardwaretable[n.hardware];
         this.pin = pintable[n.pin];
         this.set = n.set;
         this.level = n.level;
         var node = this;
-        if (node.pin>=0) 
-		  {
-           node.pi = new PIFD.PIFaceDigital(node.hardware,true); 			
-            if (node.set) 
-			  {
-              node.pi.set(node.pin,Number(node.level));             
-              } 
-            node.on("input", function(msg) {
-                if (msg.payload === "true") { msg.payload = true; }
-                if (msg.payload === "false") { msg.payload = false; }
+        if (node.pin >= 0) {
+            node.pi = new PIFD.PIFaceDigital(node.hardware, true);
+            if (node.set) {
+                node.pi.set(node.pin, Number(node.level));
+            }
+            node.on("input", function (msg) {
+                if (msg.payload === "true") {
+                    msg.payload = true;
+                }
+                if (msg.payload === "false") {
+                    msg.payload = false;
+                }
                 var out = Number(msg.payload);
-                if ((out === 0)|(out === 1)) 
-				  {
-			      node.pi.set(node.pin,Number(out)); 
-                  }
-                else 
-				  { 
-			      node.warn("Invalid input - not 0 or 1"); 
-				  }
+                if ((out === 0) | (out === 1)) {
+                    node.pi.set(node.pin, Number(out));
+                } else {
+                    node.warn("Invalid input - not 0 or 1");
+                }
             });
-          }
-        else 
-		  {
-            node.error("Invalid PiFACE pin: "+node.pin);
-			node.status({fill:"red",shape:"ring",text:"misconfigured"},true);
+        } else {
+            node.error("Invalid PiFACE pin: " + node.pin);
+            node.status({fill: "red", shape: "ring", text: "misconfigured"}, true);
         }
     }
 
-        RED.nodes.registerType("rpi-piface-digital in",PiFACEDigitalInNode);
-        RED.nodes.registerType("rpi-piface-digital out",PiFACEDigitalOutNode);
+    RED.nodes.registerType("rpi-piface-digital in", PiFACEDigitalInNode);
+    RED.nodes.registerType("rpi-piface-digital out", PiFACEDigitalOutNode);
 }
